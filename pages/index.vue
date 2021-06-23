347b292f1beb4f8842963bbe761609c6b255a51c
Roman Thaler <roman.thaler@adesso.at>
2021-06-23 23:12:05 +0200

    initial project commit

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+  computed: {
+    ...mapState(['events']),
+  },
+
+  created() {
+    this.getEvents();
+  },
+
+  methods: {
+    ...mapActions(['getEvents']),
+  },
+};
+</script>
+<template>
+  <div class="max-w-screen-lg mx-auto p-10">
+    <div v-for="(event, index) in events" :key="event.id">
+      <div :key="index" class="lg:flex lg:max-w-screen-lg pb-8 lg:pb-16">
+        <div class="lg:w-1/4">
+          <img
+            v-if="event.acf.image"
+            :src="event.acf.image.sizes.large"
+            :alt="event.acf.image.alt"
+            class="w-64 h-64 object-cover mb-4 lg:mb-0"
+          />
+        </div>
+        <div class="lg:w-3/4 lg:pl-8">
+          <h4 class="text-xl lg:text-3xl font-normal leading-tight">
+            {{ event.title.rendered }}
+          </h4>
+          <h3 class="lg:text-2xl font-bold mb-2">
+            {{ event.acf.speaker }}
+          </h3>
+          <time class="text-sm lg:text-lg font-mono block mb-2">
+            {{ event.acf.start_time }} - {{ event.acf.end_time }}
+          </time>
+          <p class="mb-4" v-html="event.content.rendered"></p>
+          <nuxt-link :to="`/events/${event.slug}`" class="btn-sm lg:btn btn-green mb-2 mr-2">
+            Event Info
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
